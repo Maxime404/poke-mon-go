afe937b1d48475b48e422be1b86fa83d827773a3
Maxime404 <galissairemaxime24@gmail.com>
2020-10-15 17:45:03 +0200

    Add padding on images

--- poke-mon-go-client/src/PokeCard.js
+++ poke-mon-go-client/src/PokeCard.js
@@ -5,7 +5,7 @@ function PokeCard(props) {
     return (
         <a href={`/${props.num}`}>
             <div class="pokemon-pcard">
-                <img src={props.image} alt={props.num} />
+                <img class="p-2" src={props.image} alt={props.num} />
                 <p class="text-left px-3 pb-3" key={props.num}>
                     <strong>#{props.num}</strong><br />{props.name}
                 </p>
