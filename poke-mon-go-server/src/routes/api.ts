8e0c3cbcdd935a3ed829d7a889bb5c97491969ee
Maxime404 <galissairemaxime24@gmail.com>
2020-10-15 17:40:07 +0200

    Fix icones

--- poke-mon-go-server/src/routes/api.ts
+++ poke-mon-go-server/src/routes/api.ts
@@ -4,7 +4,7 @@ import pokemons from './pokemons'
 const api = Router()
 
 api.get('/', (req: Request, res: Response) => {
-    res.send('<h1>Hi, welcome to our pokedex API!</h1>')
+    res.send('<h1>Hi, welcome to our Poke-mo-go API!</h1>')
 })
 
 api.use('/pokemons', pokemons)
